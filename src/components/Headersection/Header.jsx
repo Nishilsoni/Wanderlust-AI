02784beeea9c6c10d2975fa1abb7609e87be34bf
Your Name <Your Name>
2024-12-27 14:00:08 +0530

    bug fix

--- src/components/Headersection/Header.jsx
+++ src/components/Headersection/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { auth } from '../firebase/firebase.js'; // Ensure the path to your Firebase setup is correct
+import { auth } from '../Firebase/firebase.js'; // Ensure the path to your Firebase setup is correct
 import { signOut } from 'firebase/auth'; 
 import { useNavigate } from 'react-router-dom';
 
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { auth } from './firebase/firebase';
+import { auth } from '../Firebase/firebase.js';
 import { signInWithEmailAndPassword, sendPasswordResetEmail } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
